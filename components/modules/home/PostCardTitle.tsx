0366839b33679fbef107aea419d0e74b6a14f777
Tareq <tareqnmd@gmail.com>
2025-01-24 18:17:44 +0600

    Implement copy functionality in PostCardTitle component
    - Added state management to handle copy status with a temporary message.
    - Refactored button click handler to use a callback for improved readability.
    - Updated button text and aria-label for better accessibility feedback.

--- components/modules/home/PostCardTitle.tsx
+++ components/modules/home/PostCardTitle.tsx
@@ -1,16 +1,28 @@
 'use client';
 
 import { copyText } from '@/lib/utils/common';
+import { useCallback, useState } from 'react';
 
 const PostCardTitle = ({ title }: { title: string }) => {
+	const [isCopied, setIsCopied] = useState(false);
+
+	const copy = useCallback(() => {
+		copyText(title);
+		setIsCopied(true);
+		setTimeout(() => {
+			setIsCopied(false);
+		}, 2000);
+	}, [title]);
+
 	return (
 		<div className="post-title truncate flex justify-between gap-4">
 			<h1 className="truncate text-lg font-bold text-gray-800">{title}</h1>
 			<button
-				onClick={() => copyText(title)}
+				onClick={copy}
 				className="bg-gray-600 rounded-md text-white px-2 py-1"
+				aria-label={isCopied ? 'Text copied' : 'Copy text'}
 			>
-				Copy
+				{isCopied ? 'Copied!' : 'Copy'}
 			</button>
 		</div>
 	);
